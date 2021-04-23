3c05dace16e7e8b85bb243c4f06891d7d32e65d3
arturkolaska <kolaska128@gmail.com>
2021-04-23 08:17:27 +0200

    Add new constants

---
+++ src/main/java/com/github/arturkolaska/reposapi/constants/NumericConstants.java
@@ -0,0 +1,7 @@
+package com.github.arturkolaska.reposapi.constants;
+
+import java.time.Duration;
+
+public class NumericConstants {
+    public static final Duration CONN_TIMEOUT = Duration.ofMillis(10_000);
+}
--- src/main/java/com/github/arturkolaska/reposapi/constants/StringConstants.java
+++ src/main/java/com/github/arturkolaska/reposapi/constants/StringConstants.java
@@ -3,4 +3,6 @@ package com.github.arturkolaska.reposapi.constants;
 public class StringConstants {
     public static final String GITHUB_API_BASE_URI = "https://api.github.com";
     public static final String GITHUB_API_REPOS_URI = "/users/{username}/repos";
+    public static final String TOTAL_STARS_KEY = "total_stargazers_count";
+    public static final String WEBCLIENT_TIMEOUT = "Request timeout: the server has decided to close the connection.";
 }
