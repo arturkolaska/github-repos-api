c855d71ed8e47695d52a7086792c7ae732830378
arturkolaska <kolaska128@gmail.com>
2021-04-24 09:20:40 +0200

    Create ControllerTest

---
+++ src/test/java/com/github/arturkolaska/reposapi/controller/RepositoryControllerTest.java
@@ -0,0 +1,21 @@
+package com.github.arturkolaska.reposapi.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepositoryControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllRepositories() {
+    }
+
+    @Test
+    void getTotalStars() {
+    }
+}
