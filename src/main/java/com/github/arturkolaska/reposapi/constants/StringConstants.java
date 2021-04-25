3365de4ef59910161c3839c60d8d58c6d1ecdd3a
arturkolaska <kolaska128@gmail.com>
2021-04-25 16:46:36 +0200

    Fix a typo

--- src/main/java/com/github/arturkolaska/reposapi/constants/StringConstants.java
+++ src/main/java/com/github/arturkolaska/reposapi/constants/StringConstants.java
@@ -6,8 +6,8 @@ public class StringConstants {
 
     public static final String USER_REPOS_KEY = "user_repositories_url";
     public static final String REPOS_URI = "/v1/{username}/repos";
-    public static final String TOTAL_STARS_URI = "/v1/{username}/repos/total-stars";
     public static final String USER_TOTAL_STARS_KEY = "user_total_stars_url";
+    public static final String TOTAL_STARS_URI = "/v1/{username}/repos/total-stars";
 
     public static final String TOTAL_STARS_KEY = "total_stargazers_count";
 }
